d9770736783a94e3af1eaef9f7ea00ff767f59da
Artem Romanov <artem.romanov@abz.agency>
2023-06-05 16:31:23 +0300

    init

---
+++ public/component---src-pages-index-js-a761b77ca5e67a1211fb.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcf_static_page=self.webpackChunkcf_static_page||[]).push([[678],{6558:function(e,t,a){a.r(t),a.d(t,{Head:function(){return n}});var c=a(7294);t.default=()=>c.createElement("div",null,"HELLO");const n=()=>c.createElement("title",null,"Home Page")}}]);
+//# sourceMappingURL=component---src-pages-index-js-a761b77ca5e67a1211fb.js.map
